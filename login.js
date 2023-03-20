1029bc6e023bdccaeec9fc7011820269a04a01dc
Pipit Pitria <127165789+pipitpitria07@users.noreply.github.com>
2023-03-20 07:25:21 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,34 @@
+function hitung() {
+      var angka1 = (document.getElementById("angka1").value);
+      var angka1 = (document.getElementById("angka2").value);
+      var hasil = (document.getElementById("hasil").value);
+   
+function tambah(){ 
+      angka1=(document.getElementById("angka1").value);
+      angka2=(document.getElementById("angka2").value);
+      hasil=angka1 + angka2;
+      document.getElementById("hasil").value;
+   }
+
+function kurang(){ 
+      angka1=document.getElementById("a").value;
+      angka2=document.getElementById("b").value;
+      hasil=angka1 - angka2;
+      document.getElementById("hasil").value;
+   }
+
+function kali(){ 
+      angka1=document.getElementById("angka1").value;
+      angka2=document.getElementById("angka2").value;
+      hasil=angka1 * angka2;
+      document.getElementById("hasil").value;
+   }
+
+function bagi(){ 
+      angka1=document.getElementById("angka1").value;
+      angka2=document.getElementById("angka2").value;
+      hasil=angka1 / angka2;
+      document.getElementById("hasil").value;
+   }
+
+}
